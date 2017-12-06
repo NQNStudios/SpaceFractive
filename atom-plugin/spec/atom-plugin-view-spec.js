e4778cccecfbc455fb4eeb3fcdc518991196e886
Nathaniel Nelson <nelson.nleroy@gmail.com>
2017-12-05 22:41:39 -0700

    Generated atom plugin skeleton

---
+++ atom-plugin/spec/atom-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPluginView from '../lib/atom-plugin-view';
+
+describe('AtomPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
